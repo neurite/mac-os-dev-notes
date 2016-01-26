2c48e89353bdb14850241fb810d295b238de167b
neurite <neurite@users.noreply.github.com>
2016-01-26 06:19:45 -0800

    Clone previous environment when create

--- python.sh
+++ python.sh
@@ -14,22 +14,21 @@ echo '# python' >> ~/.bashrc
 
 ### IPython
 cd
-conda create -y -n ipython python=2
+conda create --yes --name ipython python=2
 source activate ipython
-conda install -y -c anaconda ipython-notebook
+conda install --yes --channel anaconda ipython-notebook
 source deactivate
 echo 'alias env-ipython="source activate ipython"' >> ~/.bashrc
-conda create -y -n ipython3 python=3
+conda create --yes --name ipython3 python=3
 source activate ipython3
-conda install -y -c ananconda ipython-notebook
+conda install --yes --channel ananconda ipython-notebook
 source deactivate
 echo 'alias env-ipython3="source activate ipython3"' >> ~/.bashrc
 
 ### The SciPy Stack
 cd
-conda create -y -n scipy python=2
+conda create -y -n scipy --clone ipython
 source activate scipy
-conda install -y -c anaconda ipython-notebook
 conda install -y -c anaconda numpy
 conda install -y -c anaconda scipy
 conda install -y -c anaconda matplotlib
@@ -37,9 +36,8 @@ conda install -y -c anaconda pandas
 conda install -y -c anaconda sympy
 source deactivate
 echo 'alias env-scipy="source activate scipy"' >> ~/.bashrc
-conda create -y -n scipy3 python=3
+conda create -y -n scipy3 --clone ipython3
 source activate scipy3
-conda install -y -c anaconda ipython-notebook
 conda install -y -c anaconda numpy
 conda install -y -c anaconda scipy
 conda install -y -c anaconda matplotlib
@@ -50,12 +48,12 @@ echo 'alias env-scipy3="source activate scipy3"' >> ~/.bashrc
 
 ### scikit-learn
 cd
-conda create -y -n sklearn python=2
+conda create -y -n sklearn --clone scipy
 source activate sklearn
 conda install -y -c anaconda scikit-learn
 source deactivate
 echo 'alias env-sklearn="source activate sklearn"' >> ~/.bashrc
-conda create -y -n sklearn3 python=3
+conda create -y -n sklearn3 --clone scipy3
 source activate sklearn3
 conda install -y -c anaconda scikit-learn
 source deactivate
